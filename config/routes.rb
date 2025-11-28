9652102bb5e04cd4ebb41817cb99194c1af854eb
Ping Wang <pingwang715@gmail.com>
2025-11-28 16:41:27 +0100

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists, only: [:index, :show, :new, :create, :edit, :update] do
+  root to: "lists#index"
+  resources :lists, only: [:show, :new, :create, :edit, :update] do
     resources :bookmarks, only: [:new, :create,]
   end
 
--- db/migrate/20251127101411_create_movies.rb
+++ db/migrate/20251127101411_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[7.1]
   def change
-    create_table :movies do j j|t|
+    create_table :movies do |t|
       t.string :title
       t.string :overview
       t.string :poster_url
